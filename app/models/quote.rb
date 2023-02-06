566df8d28218c883ef2b7d0b6886cd8188d0616a
wendellopes <wendelopes@msn.com>
2023-02-06 11:50:21 -0300

    create authentication company for restrict quote to company and user

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,7 +5,9 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_later_to 'quotes' }
-  after_update_commit -> { broadcast_replace_later_to 'quotes' }
-  after_destroy_commit -> { broadcast_remove_to 'quotes' }
+  # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
+  # after_update_commit -> { broadcast_replace_later_to 'quotes' }
+  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
+
+  broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
 end
