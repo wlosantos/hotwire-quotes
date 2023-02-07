394a1777473312dd7c3bac563be13bca2021bd94
wendellopes <wendelopes@msn.com>
2023-02-07 07:02:11 -0300

    create page line_item and controller

---
+++ app/controllers/line_item_dates_controller.rb
@@ -0,0 +1,34 @@
+class LineItemDateController < ApplicationController
+  before_action :set_quote
+
+  def new
+    @line_item_date = LineItemDate.build
+  end
+
+  def create
+    @line_item_date = LineItemDate.build(line_item_date_params)
+    if @line_item_date.save
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: 'Date was successfully created.' }
+        # format.turbo_stream do
+        #   flash.now[:notice] = {
+        #     title: 'Success',
+        #     message: 'Line Item Date was successfully created.'
+        #   }
+        # end
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def line_item_date_params
+    params.require(:line_item_date).permit(:date)
+  end
+
+  def set_quote
+    @quote = current_company.quotes.find(params[:quote_id])
+  end
+end
