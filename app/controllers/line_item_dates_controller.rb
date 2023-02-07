11d014c0a46fd078744ff61f5cb16b58d3facc21
wendellopes <wendelopes@msn.com>
2023-02-07 08:14:05 -0300

    create page new and form to line_item_date

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -1,12 +1,12 @@
-class LineItemDateController < ApplicationController
+class LineItemDatesController < ApplicationController
   before_action :set_quote
 
   def new
-    @line_item_date = LineItemDate.build
+    @line_item_date = @quote.line_item_dates.build
   end
 
   def create
-    @line_item_date = LineItemDate.build(line_item_date_params)
+    @line_item_date = @quote.line_item_dates.build(line_item_date_params)
     if @line_item_date.save
       respond_to do |format|
         format.html { redirect_to quote_path(@quote), notice: 'Date was successfully created.' }
