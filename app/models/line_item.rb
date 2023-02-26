c086d40c7708ba0b4e0f3c4e88a6616ee16dd90c
wendellopes <wendelopes@msn.com>
2023-02-25 22:06:22 -0300

    adjust limit words

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,13 +1,23 @@
 class LineItem < ApplicationRecord
   belongs_to :line_item_date
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 60 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :unit_price, presence: true, numericality: { greater_than: 0.0 }
+
+  validate :word_is_long, :descript_is_long
 
   delegate :quote, to: :line_item_date
 
   def total_price
     quantity * unit_price
+  end
+
+  private
+
+  def word_is_long
+    return unless name.present? && name.length > 10 && !name.include?(' ')
+
+    errors.add(:name, 'word invalid')
   end
 end
